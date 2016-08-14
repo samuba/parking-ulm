666fd81d66e95ac6587f85887acc5914c9560ad9
samuba <samuel.z.bach@gmail.com>
2016-08-14 16:27:43 +0200

    Updated at hyperdev.com

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 let express = require('express');
+let bodyParser = require('body-parser');
 let bot = require("./telegramBot")
 let parkdeckFetcher = require("./parkdeckFetcher")
  
 let app = express();
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 parkdeckFetcher.fetch()
 
@@ -18,8 +20,9 @@ app.get("/json", (req, resp) => {
   resp.send(parkdeckFetcher.decks);
 });
  
-app.post(process.env.TELEGRAM_BOT_TOKEN, (req, resp) => {
-  console.log("######## yo da kams", req)
+app.post("/" + process.env.TELEGRAM_BOT_TOKEN, (req, resp) => {
+  bot.processUpdate(req.body)
+  resp.send(200)
 })
 
 var listener = app.listen(process.env.PORT, function () { 
--- telegramBot.js
+++ telegramBot.js
@@ -2,22 +2,35 @@
 let TelegramBot = require('node-telegram-bot-api');
 let parkdeckFetcher = require("./parkdeckFetcher")
 
+module.exports = {
+  processUpdate: update => bot.processUpdate(update)
+}
+
 let bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {webHook: true});
+bot.setWebHook(process.env.TELEGRAM_WEBHOOK_URL)
 
-bot.setWebHook("https://thunder-jester.hyperdev.space/" + process.env.TELEGRAM_BOT_TOKEN)
+let startText = `
+Hi 😃
+Ich will dir helfen schneller einen Parkplatz in Ulm zu finden.
+Diese Kommandos kannst du benutzen um mit mir zu sprechen:
+/free - Freie Plätze in allen Parkhäusern anzeigen
+/details - Details zu den Parkhäusern anzeigen
+`
 
-bot.onText(/\/start/, (msg, match) => {
-  let reply = "/all - Freie Plätze in allen Parkhäusern anzeigen"
-  bot.sendMessage(msg.from.id, reply);
+bot.onText(/\/start$/, (msg, match) => {
+  bot.sendMessage(msg.from.id, startText);
 });
 
-bot.onText(/\/all/, (msg, match) => {
+bot.onText(/\/help$/, (msg, match) => {
+  bot.sendMessage(msg.from.id, startText);
+});
+
+
+bot.onText(/\/all$/, (msg, match) => {
   let reply = ""
-  parkdeckFetcher.decks.forEach(deck => {
-    reply += deck.name + ": " + deck.free + ` ${deck} /location` + deck.id +"\n"
-  })
+  parkdeckFetcher.decks.forEach(x => reply += `${x.name}: ${x.free} /details${x.id}\n`)
   bot.sendMessage(msg.from.id, reply);
-});
+}); 
 
 bot.onText(/\/location(.+)/, (msg, match) => {
   let id = match[1];
@@ -25,10 +38,42 @@ bot.onText(/\/location(.+)/, (msg, match) => {
   bot.sendLocation(msg.from.id, deck.location.lat, deck.location.long);
 });
 
-console.log("bot running") 
-
-module.exports = {
-  run: function () {
-    
+bot.onText(/\/details$/, (msg, match) => {
+  let reply = "Welches Parkhaus interessiert dich?"
+  let keyboard = []
+  let decks = parkdeckFetcher.decks
+  for (let i=0; i<=decks.length; i+=2) {
+    let buttons = []
+    if (decks[i]) {
+      buttons.push(`/details${decks[i].id}`)
+    }
+    if (decks[i+1]) {
+      buttons.push(`/details${decks[i+1].id}`)
+    }
+    keyboard.push(buttons)
   }
-}
+  bot.sendMessage(msg.from.id, reply, { reply_markup: {
+    one_time_keyboard: true,
+    keyboard: keyboard
+  }})
+});
+
+bot.onText(/\/details(.+)/, (msg, match) => {
+  let id = match[1];
+  let deck = parkdeckFetcher.decks[parkdeckFetcher.getDeckIndex(id)]
+  let reply = `
+*${deck.name}*
+Frei: ${deck.free}
+Kapazität: ${deck.total}
+Öffnungszeiten: ${deck.openText}
+`
+  bot.sendMessage(msg.from.id, reply, { 
+    parse_mode: "Markdown", 
+    reply_markup: { hide_keyboard: true }
+  })
+  setTimeout(() => {
+      bot.sendLocation(msg.from.id, deck.location.lat, deck.location.long);
+  }, 100)
+});
+
+console.log("bot running") 
