6b7aef025fdcc3c31647ad6b45a1d7bc54f619d0
samuba <samuel.z.bach@gmail.com>
2016-08-15 20:35:47 +0200

    Updated at hyperdev.com

--- server.js
+++ server.js
@@ -2,30 +2,34 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let bot = require("./telegramBot")
-let parkdeckFetcher = require("./parkdeckFetcher")
- 
+let pdFetcher = require("./parkdeckFetcher")
+
+pdFetcher.fetch()
+setInterval(() => pdFetcher.fetch(), 30 * 1000)
+
 let app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
-
-parkdeckFetcher.fetch()
-
-setInterval(() => parkdeckFetcher.fetch(), 2 * 60 * 1000);
-
+ 
 app.get("/", (req, resp) => {
   resp.sendFile(__dirname + '/views/index.html');
-});
+})
 
 app.get("/json", (req, resp) => {
-  resp.send(parkdeckFetcher.decks);
-});
+  resp.send(pdFetcher.decks);
+})
  
 app.post("/" + process.env.TELEGRAM_BOT_TOKEN, (req, resp) => {
-  bot.processUpdate(req.body)
+  if (pdFetcher.decks[0].free === undefined) {
+    // container just started. warit for parkdeckfetch to finish
+    // otherwise we wont have data available
+    setTimeout(() => pdFetcher.fetch(), 2 * 1000)
+  }
+  bot.processUpdate(req.body) 
   resp.send(200)
 })
 
-var listener = app.listen(process.env.PORT, function () { 
+var listener = app.listen(process.env.PORT, () => { 
   console.log('Your app is listening on port ' + listener.address().port);
-});
+})
 
--- telegramBot.js
+++ telegramBot.js
@@ -1,6 +1,6 @@
 'use strict';
 let TelegramBot = require('node-telegram-bot-api');
-let parkdeckFetcher = require("./parkdeckFetcher")
+let pdFetcher = require("./parkdeckFetcher")
 
 module.exports = {
   processUpdate: update => bot.processUpdate(update)
@@ -9,6 +9,14 @@ module.exports = {
 let bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {webHook: true});
 bot.setWebHook(process.env.TELEGRAM_WEBHOOK_URL)
 
+let defaultMsgParams = { 
+  parse_mode: "Markdown",
+  reply_markup: { 
+    keyboard: [['/free', '/details']],
+    resize_keyboard: true
+  }
+}
+
 let startText = `
 Hi 😃
 Ich will dir helfen schneller einen Parkplatz in Ulm zu finden.
@@ -16,55 +24,48 @@ Diese Kommandos kannst du benutzen um mit mir zu sprechen:
 /free - Freie Plätze in allen Parkhäusern anzeigen
 /details - Details zu den Parkhäusern anzeigen
 `
-
 bot.onText(/\/start$/, (msg, match) => {
-  bot.sendMessage(msg.from.id, startText);
+  bot.sendMessage(msg.from.id, startText,  defaultMsgParams);
 });
 
 bot.onText(/\/help$/, (msg, match) => {
-  bot.sendMessage(msg.from.id, startText);
+  bot.sendMessage(msg.from.id, startText, defaultMsgParams);
 });
 
 
 bot.onText(/\/free$/, (msg, match) => {
-  let reply = ""
-  parkdeckFetcher.decks.forEach(x => reply += `${x.name}: ${x.free} /details${x.id}\n`)
+  let reply = "So viele Parkplätze sind noch in den jeweiligen Parkhäusern frei:\n\n"
+  pdFetcher.decks.forEach(x => reply += `${x.name}: ${x.free} /details${x.id}\n`)
   bot.sendMessage(msg.from.id, reply);
 }); 
 
 bot.onText(/\/details$/, (msg, match) => {
   let reply = "Welches Parkhaus interessiert dich?"
   let keyboard = []
-  let decks = parkdeckFetcher.decks
+  let decks = pdFetcher.decks
   for (let i=0; i<=decks.length; i+=2) {
     let buttons = []
-    if (decks[i]) {
-      buttons.push(`/details${decks[i].id}`)
-    }
-    if (decks[i+1]) {
-      buttons.push(`/details${decks[i+1].id}`)
-    }
+    if (decks[i]) buttons.push(`/details${decks[i].id}`)
+    if (decks[i+1]) buttons.push(`/details${decks[i+1].id}`)
     keyboard.push(buttons)
   }
   bot.sendMessage(msg.from.id, reply, { reply_markup: {
     one_time_keyboard: true,
+    resize_keyboard: true,
     keyboard: keyboard
   }})
 });
 
 bot.onText(/\/details(.+)/, (msg, match) => {
   let id = match[1];
-  let deck = parkdeckFetcher.decks[parkdeckFetcher.getDeckIndex(id)]
+  let deck = pdFetcher.decks[pdFetcher.getDeckIndex(id)]
   let reply = `
 *${deck.name}*
 Frei: ${deck.free}
 Kapazität: ${deck.total}
 Öffnungszeiten: ${deck.openText}
 `
-  bot.sendMessage(msg.from.id, reply, { 
-    parse_mode: "Markdown", 
-    reply_markup: { hide_keyboard: true }
-  })
+  bot.sendMessage(msg.from.id, reply, defaultMsgParams)
   setTimeout(() => {
       bot.sendLocation(msg.from.id, deck.location.lat, deck.location.long);
   }, 100)
